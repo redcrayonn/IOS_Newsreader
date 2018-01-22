962f76a829465bb39d88e16ff16db04eb47a39d3
Dev Olaf <devolaf@devs-mac.local>
2018-01-22 07:22:23 -0800

    initial

---
+++ IOS Newsreader/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  IOS Newsreader
+//
+//  Created by Dev Olaf on 22/01/2018.
+//  Copyright © 2018 Dev Olaf. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
